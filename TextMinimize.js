2dda7f6420b687e7a5b2d91faa2c72db33b6f740
Nitesh Shrestha <nitesh.shresthax@gmail.com>
2020-01-24 10:32:04 +0545

    Word_Wrap_Fix

--- TextMinimize.js
+++ TextMinimize.js
@@ -1,34 +1,39 @@
 import React, { useState } from 'react';
 
 function TextMinimize(props) {
-	const initialText = props.text;
-	const characters = props.characters;
-	const Tag = props.as;
-	const text =
-		initialText.length > characters ? initialText.slice(0, characters) + '...' : initialText;
+  const initialText = props.text;
+  const characters = props.characters;
+  const Tag = props.as;
+  const text =
+    initialText.length > characters
+      ? initialText.slice(0, characters) + '...'
+      : initialText;
 
-	const [currentText, setCurrentText] = useState(text);
-	const [readState, setReadState] = useState(false);
+  const [currentText, setCurrentText] = useState(text);
+  const [readState, setReadState] = useState(false);
 
-	const toggleText = () => {
-		if (!readState) {
-			setCurrentText(initialText);
-		} else {
-			setCurrentText(text);
-		}
-		setReadState(!readState);
-	};
+  const toggleText = () => {
+    if (!readState) {
+      setCurrentText(initialText);
+    } else {
+      setCurrentText(text);
+    }
+    setReadState(!readState);
+  };
 
-	return (
-		<Tag>
-			{currentText}{' '}
-			{initialText.length > characters && (
-				<span onClick={toggleText} style={{ color: '#000', cursor: 'pointer' }}>
-					{readState ? 'Read Less' : 'Read More'}
-				</span>
-			)}
-		</Tag>
-	);
+  return (
+    <Tag>
+      <span style={{ wordWrap: 'break-word' }}>{currentText} </span>
+      {initialText.length > characters && (
+        <span
+          onClick={toggleText}
+          style={{ color: '#000', cursor: 'pointer', wordWrap: 'break-word' }}
+        >
+          {readState ? 'Read Less' : 'Read More'}
+        </span>
+      )}
+    </Tag>
+  );
 }
 
 export default TextMinimize;
