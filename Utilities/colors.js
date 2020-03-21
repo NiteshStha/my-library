24396e87d7a70ea977f2b638336c5a71400ff45f
Nitesh Shrestha <nitesh.shresthax@gmail.com>
2020-03-21 23:13:44 +0545

    Added Colors 🦋

---
+++ Utilities/colors.js
@@ -0,0 +1,7 @@
+const colors = {
+  colorful_and_balanced: ['#e12d60', '#85dcb0', '#e8a87c', '#c38d9e', '#41b3a3'],
+  vibrant_and_elegant: ['#f8e9a1', '#f76c6c', '#a8d0e6', '#374785', '#24305e'],
+  cheerful_and_energetic: ['#fbe8a6', '#f4976c', '#303c6c', '#b4dfe5', '#d2fdff'],
+  rich_and_colorful: ['#f78888', '#f3d250', '#ececec', '#90ccf4', '#5da2d5'],
+  intrepid_and_fearless: ['#314455', '#644e5b', '#9e5a63', '#c96567', '#97aabd']
+};
