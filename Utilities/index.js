0d359e8994ad594b6d242067b99ebe91658818c1
Nitesh Shrestha <nitesh.shresthax@gmail.com>
2020-04-10 15:14:40 +0545

    added number generator in index

--- Utilities/index.js
+++ Utilities/index.js
@@ -1,9 +1,11 @@
 import { NumberGenerator } from './NumberGenerator';
 import { GroupArrayByKey } from './GroupArraysByKey';
+import { NumberGenerator } from './NumberGenerator';
 
 const Utilities = {
-	NumberGenerator,
-	GroupArrayByKey,
+  NumberGenerator,
+  GroupArrayByKey,
+  NumberGenerator,
 };
 
 export default Utilities;
